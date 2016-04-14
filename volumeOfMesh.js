7b6cb8a12e898fb29c68a86ee428030c1e8882e0
Karen Hao <karendhao@gmail.com>
2016-04-13 22:57:11 -0700

    cleaned up code for volumeOfMesh block

--- volumeOfMesh.js
+++ volumeOfMesh.js
@@ -4,33 +4,13 @@
  * Code block template.
  *
  */
-
- var vec3 = {
-    create: function create() {
-    	return {x:0, y:0, z:0}
-    },
-    set: function set(vector, x, y, z) {
-    	vector.x=x;
-    	vector.y=y;
-    	vector.z=z;
-    },
-    cross: function cross(vector, a, b) {
-        this.set(vector,
-        	a.y * b.z - a.z * b.y,
-        	a.z * b.x - a.x * b.z,
-        	a.x * b.y - a.y * b.x);
-    },
-    dot: function dot(a, b) {
-    	return  a.x * b.x + a.y * b.y + a.z * b.z;
-    },
-}
  
 function tetraVolume(vertices) {
-	var p1 = vertices[0]
-	var p2 = vertices[1]
-	var p3 = vertices[2]
+    var p1 = vertices[0]
+    var p2 = vertices[1]
+    var p3 = vertices[2]
 	
-	var v321 = p3[0]*p2[1]*p1[2];
+    var v321 = p3[0]*p2[1]*p1[2];
     var v231 = p2[0]*p3[1]*p1[2];
     var v312 = p3[0]*p1[1]*p2[2];
     var v132 = p1[0]*p3[1]*p2[2];
@@ -42,28 +22,30 @@ function tetraVolume(vertices) {
 }
 
 function run(mesh) {
-	var totalVolume = 0;
-	var faces = mesh.faces;
-	var vertices = mesh.vertices;
-	
-	faces.forEach(function(face) {
+    var totalVolume = 0;
+    var faces = mesh.faces;
+    var vertices = mesh.vertices;
+
+    faces.forEach(function(face) {
 	    var points = face.map(function(index) {
 	        return vertices[index];
 	    });
 	    
 	    if (points.length == 4) {
-	    	var points1 = [points[0],points[1],points[2]];
-	    	var points2 = [points[2],points[3],points[0]];
+	        var points1 = [points[0],points[1],points[2]];
+	        var points2 = [points[2],points[3],points[0]];
 
-	    	totalVolume += tetraVolume(points1) + tetraVolume(points2);
+	        totalVolume += tetraVolume(points1) + tetraVolume(points2);
 	    } else if (points.length == 3) {
-	    	totalVolume += tetraVolume(points);
+	        totalVolume += tetraVolume(points);
+	    } else {
+	        throw new Error("encountered mesh face with 5 or more points, can't calculate volume.");
 	    }
-	});
+    });
 	
 	
 return {
-	volume: totalVolume
+    volume: totalVolume
 };
 
 }
