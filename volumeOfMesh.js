97da1b9ae5bd53bdca9605bc3aa759414774633b
Karen Hao <karenhao@Karens-MacBook-Pro.local>
2016-04-13 18:00:40 -0700

    added volumeOfMesh block

---
+++ volumeOfMesh.js
@@ -0,0 +1,14 @@
+var vec3 = {
+    create: function create() {
+    
+    },
+    set: function set(vector, x, y, z) {
+    
+    },
+    cross: function cross(vector1, vector2) {
+        
+    },
+    dot: function dot(vector1, vector2) {
+    
+    },
+}
