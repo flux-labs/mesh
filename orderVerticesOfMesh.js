ea502eaf6d151602c61b1a2308eeb9293c1ffded
CharlieP <charlie@flux.io>
2016-05-10 13:55:27 -0400

    Formatting

--- orderVerticesOfMesh.js
+++ orderVerticesOfMesh.js
@@ -1,14 +1,16 @@
 'use strict';
 
 /**
- * Code block template.
- *
+ * Order the verticies of a mesh clockwise
+ * @param {Mesh} Mesh object
+ * 
+ * @return {Vertices} ordered vertices
  */
 var modeling = require('flux/modeling');
 
-function run(mesh) {
-	var faces = mesh[0].faces;
-	var vertices = mesh[0].vertices;
+function run(Mesh) {
+	var faces = Mesh[0].faces;
+	var vertices = Mesh[0].vertices;
 	var points;
 	var listOfPoints=[];
  	faces.forEach(function(face) {
@@ -19,7 +21,7 @@ function run(mesh) {
  	});
 
 return {
-    orderedVertices: listOfPoints
+    Vertices: listOfPoints
 };
 
 }
